31455120ddcf7660cb6a22b3e2b2eccc6aa20a35
SusanSRS <susan.shakya24@gmail.com>
2022-09-15 10:50:28 +0545

    bootstrap

---
+++ src/component/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='container-fluid susan'>
+            <div className="container">
+                <div className="row text-white">
+              <div className="col-md-3">
+                  logo here
+              </div>
+              <div className="col-md-3">
+                  logo here
+              </div>
+              <div className="col-md-3">
+                  logo here
+              </div>
+              <div className="col-md-3">
+                  logo here
+              </div>
+          </div>
+          </div>
+
+    </div>
+  )
+}
+
+export default Footer
